cdf1f268b642762cf066e0f7fe5153ca79f8fd53
Alikhan <jonasofan.gubaev@gmail.com>
2023-03-27 23:23:17 +0600

    rock-scissors-paper first commit

---
+++ rock-scissors-paper/rock-scissors-paper/main.swift
@@ -0,0 +1,8 @@
+print("Game maded by Alikhan Gubayev on nFactorial iOS Course")
+print("----------------------")
+
+print("Hello to the Game: Rock, Scissors, Paper.")
+print("With whom you want to play?")
+print("1 - with computer.")
+print("2 - with friend.")
+
